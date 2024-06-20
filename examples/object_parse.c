6e888fa708cdb91b37362755f4415b80c26b4b4a
= <=>
2024-06-20 23:13:35 +0100

    Updated

--- examples/object_parse.c
+++ examples/object_parse.c
@@ -7,7 +7,7 @@ int main(void) {
   HMON_Object *object = NULL;
 
   // Parse string to object
-  hmon_parse_string_object(&object, "(username = 'Hatim', password = 'Somepass123', email = 'hatim.withred@gmail.com')");
+  hmon_parse_string_object(&object, "(username = 'Hatim', password = 'Somepass123', email = 'hatim@gmail.com')");
 
   hmon_object_print(&object);
   hmon_object_destroy(&object);
